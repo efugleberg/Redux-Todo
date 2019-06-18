9155ea789ea68700ce3f97ab39a866dbc047d1ff
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-17 21:41:14 -0500

    WIP: Commit from Monday evening

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { todos } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todos)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 
